745168bdad61efbf411feb824c45760948a95b3c
Geovanna Barbosa <geovannab177@gmail.com>
2026-09-21 19:01:17 -0300

    Remove mobile bottom navigation

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,16 +1,14 @@
 import { Sidebar } from '@/components/nav/Sidebar';
-import { BottomNav } from '@/components/nav/BottomNav';
 
 export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-dvh bg-paper">
       <Sidebar />
       <div className="lg:pl-[270px]">
-        <main className="mx-auto min-h-dvh w-full max-w-[1480px] px-4 pb-28 pt-5 sm:px-6 lg:px-10 lg:pb-10 lg:pt-8">
+        <main className="mx-auto min-h-dvh w-full max-w-[1480px] px-4 pb-6 pt-5 sm:px-6 lg:px-10 lg:pb-10 lg:pt-8">
           {children}
         </main>
       </div>
-      <BottomNav />
     </div>
   );
 }
