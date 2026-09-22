ed5a58a176277b6d3ce6a19bcd5a9ff69bcb9833
Geovanna Barbosa <geovannab177@gmail.com>
2026-09-21 21:50:07 -0300

    Ajustar layout responsivo da Secretária

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -4,8 +4,8 @@ export default function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-h-dvh bg-paper">
       <Sidebar />
-      <div className="lg:pl-[270px]">
-        <main className="mx-auto min-h-dvh w-full max-w-[1480px] px-4 pb-6 pt-5 sm:px-6 lg:px-10 lg:pb-10 lg:pt-8">
+      <div className="lg:pl-[252px]">
+        <main className="mx-auto min-h-dvh w-full max-w-[1440px] px-4 pb-8 pt-[76px] sm:px-6 sm:pt-8 lg:px-8 lg:pb-12 lg:pt-8 xl:px-10">
           {children}
         </main>
       </div>
